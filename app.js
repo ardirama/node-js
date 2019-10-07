95943fc6dd98951c69b80f742e75992d9305e8f1
rianGans <rianfriwan26@gmail.com>
2019-10-07 09:26:41 +0700

    Mengubah app.js dengan materi serving static file

--- app.js
+++ app.js
@@ -1,15 +1,28 @@
+// menggunakan path module
+const path = require('path');
+// menggunakan express module 
 const express = require('express');
+// menggunakan hbs view engine
+const hbs = require('hbs');
 const app = express();
 
-// route untuk halamam home
+
+// konfigurasi dynamic views file
+app.set('views', path.join(__dirname, 'views'));
+// konfigurasi view engine
+app.set('view engine', 'hbs');
+// konfigurasi folder publik menjadi statis folder untuk file statis
+app.use(express.static('public'));
+// route untuk halaman Home
 app.get('/', (req,res) => {
-	res.send('welcome To Home');
+	res.render('index');
 });
 
+// route untuk halaman about 
 app.get('/about', (req,res) => {
-	res.end('This is a About Page');
+	res.render('about');
 });
 
-app.listen(8080, () => {
-	console.log('server on running at port 8080');
+app.listen(8080, () =>{
+	console.log('server running on port 8080');
 });
